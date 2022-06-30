d53759a09d636159eae52339f831aeb67eb9100b
Anastasiia Shevtsova <anastasiia08@ukr.net>
2022-06-30 21:35:06 +0200

    fix naming

--- lesson13/task2/calculator.test.js
+++ lesson13/task2/calculator.test.js
@@ -1,7 +1,7 @@
-import getSum, { getSquaredNumbers, getOddNumbers } from './calculator.js';
+import getSum, { getSquaredArray, getOddNumbers } from './calculator.js';
 
 it('should get squared numbers', () => {
-  const result = getSquaredNumbers([1, 2, 3]);
+  const result = getSquaredArray([1, 2, 3]);
   expect(result).toEqual([1, 4, 9]);
 });
 
